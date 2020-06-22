924b029876e9b0f20f13532049a9c8d71657a89a
ZhangYizhuo <bouncyslime@126.com>
2020-06-22 23:37:30 +0800

    map的一些用法，返回及不返回

--- src/main/java/cn/bouncyslime/javaKnowledge/stream/Test2.java
+++ src/main/java/cn/bouncyslime/javaKnowledge/stream/Test2.java
@@ -20,14 +20,20 @@ public class Test2 {
 
         // 过滤出 语文成绩小于 80分的同学
         logger.info(">>>>> 以下为过滤语文成绩小于80分的学生及人数");
-        logger.debug(String.valueOf(list.stream().filter(x -> x.getChineseScore() < 80).count()));
-        logger.debug(list.stream().filter(x -> x.getChineseScore() < 80).findAny().get().toString());
-//        logger.debug(list.stream().filter(x -> x.getChineseScore() > 80).collect().toString());
+        List list1;
+        int count;
+        count = (int) list.stream().filter(x -> x.getChineseScore() < 80).count();
+        logger.debug(Integer.toString(count));
+        list1 = list.stream().filter(x -> x.getChineseScore() < 80).collect(Collectors.toList());
+        logger.debug(list1.toString());
 
         logger.info(">>>>> 以下为将所有学生的姓名放到一个List中");
-        List<String> list2 = new ArrayList<>();
+        List<String> list2;
         list2 = list.stream().map(Student::getName).collect(Collectors.toList());
         logger.debug(list2.toString());
+
+        List list3;
+//        list3 = list.stream().map(Student::getName).collect(Collectors.toCollection(() -> ));
 
         logger.info(">>>>> 以下为将所有学生按照姓名首字母顺序进行排序输出");
         list.stream().sorted((o1, o2) -> {
@@ -43,7 +49,12 @@ public class Test2 {
         logger.info((">>>>> 以下为map的一些用法"));
         logger.debug(list.stream().map(x -> x.getAge() + 10).findFirst().get().toString());
         list.stream().map(x -> x.getAge() + 10).forEach(x -> {logger.debug(x.toString());});
+        logger.debug(list.toString());
+        list.stream().map(x -> {
+            x.setName(x.getName() + " modified");
+            return x;
+        }).forEach(x -> logger.debug(x.toString()));
+        logger.debug(list.toString());
         logger.debug(String.valueOf(list.stream().mapToDouble(Student::getAge).findAny().getAsDouble()));
-
     }
 }
