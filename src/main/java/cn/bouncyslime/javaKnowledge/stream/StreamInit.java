260c467c0ea2b1df27adfce15360b8711113a464
ZhangYizhuo <bouncyslime@126.com>
2020-06-26 19:08:25 +0800

    stream init

--- src/main/java/cn/bouncyslime/javaKnowledge/stream/StreamInit.java
+++ src/main/java/cn/bouncyslime/javaKnowledge/stream/StreamInit.java
@@ -2,8 +2,11 @@ package cn.bouncyslime.javaKnowledge.stream;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -14,7 +17,7 @@ import java.util.stream.Stream;
 public class StreamInit {
     public static void main(String[] args) {
         // 1.
-        Stream stream = Stream.of("a", "b", "c");
+        Stream<String> stream = Stream.of("a", "b", "c");
         // 2.
         String[] strings = new String[] {"d", "e", "f"};
         stream = Stream.of(strings);
@@ -22,5 +25,14 @@ public class StreamInit {
         // 3.
         List<String> list = Arrays.asList(strings);
         stream = list.stream();
+
+        /*
+          以下为 数据流 ==>> 其他数据结构
+         */
+        String[] stringArray = stream.toArray(String[]::new);
+        List<String> streamToList = stream.collect(Collectors.toList());
+        List<String> streamToList2 = stream.collect(Collectors.toCollection(ArrayList::new));
+        Set<String> set = stream.collect(Collectors.toSet());
+        String string = stream.collect(Collectors.joining());
     }
 }
