60c1b7f93b2c9d548933d3ec9764e407e27921e7
bouncySlime <bouncyslime@126.com>
2020-07-20 23:33:32 +0800

    optional test，对于optional有很多疑问

---
+++ src/main/java/cn/bouncyslime/javaKnowledge/stream/OptionalTest.java
@@ -0,0 +1,24 @@
+package cn.bouncyslime.javaKnowledge.stream;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * @Author 张一卓
+ * @Date 2020/7/20
+ */
+@Slf4j
+public class OptionalTest {
+    public static void main(String[] args) {
+        String string = "abcd";
+        String string2 = null;
+
+        Optional.ofNullable(string2).ifPresent(log::debug);
+        Optional.ofNullable(string).ifPresent(log::debug);
+
+        int[] array = {1, 2, 4, 8, 3};
+        Stream.of(array).findFirst().get();
+    }
+}
