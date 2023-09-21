d3f2c3fb385cb207ddbf5c307f0c43548f00a4e8
zhuo <zhangyizhuo@sinosoft.com.cn>
2023-09-21 15:28:40 +0800

    日志修改

---
+++ src/main/java/cn/z2huo/java/knowledge/log/logback/Test.java
@@ -0,0 +1,12 @@
+package cn.z2huo.java.knowledge.log.logback;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.error("slf4j and logback success");
+    }
+
+}
