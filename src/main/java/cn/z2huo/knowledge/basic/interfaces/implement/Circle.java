f6ba9887a4f196d00bd6038fadafcfd22c1022c1
zhuo <zhangyizhuo@sinosoft.com.cn>
2023-10-12 09:31:48 +0800

    implement

---
+++ src/main/java/cn/z2huo/knowledge/basic/interfaces/implement/Circle.java
@@ -0,0 +1,15 @@
+package cn.z2huo.knowledge.basic.interfaces.implement;
+
+class Circle implements Shape{
+
+
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void len() {
+
+    }
+}
---
+++ src/main/java/cn/z2huo/knowledge/basic/interfaces/implement/Shape.java
@@ -0,0 +1,9 @@
+package cn.z2huo.knowledge.basic.interfaces.implement;
+
+interface Shape {
+
+    void draw();
+
+    void len();
+
+}
