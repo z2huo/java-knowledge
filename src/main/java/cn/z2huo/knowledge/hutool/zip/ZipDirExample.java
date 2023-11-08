54dfca5b6c614a617c6819d3399ee3be8ba8679b
zhuo <zhangyizhuo@sinosoft.com.cn>
2023-11-08 21:28:58 +0800

    hutool zip

---
+++ src/main/java/cn/z2huo/knowledge/hutool/zip/ZipDirExample.java
@@ -0,0 +1,30 @@
+package cn.z2huo.knowledge.hutool.zip;
+
+import cn.hutool.core.util.ZipUtil;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.zip.ZipOutputStream;
+
+public class ZipDirExample {
+    public static void main(String[] args) {
+
+        try (InputStream inputStream = new FileInputStream("D:\\Desktop\\vibdata.xlsx");
+             InputStream inputStream2 = new FileInputStream("D:\\Desktop\\vibdata_处理后.xlsx");
+             ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream("D:\\Desktop\\ZIP.zip"))) {
+
+
+            ZipUtil.zip(zipOutputStream, new String[]{"dir_1/vibdata_1.xlsx", "dir_2/vibdata_2.xlsx"},
+                    new InputStream[]{inputStream, inputStream2});
+
+
+        } catch (Exception e) {
+
+        }
+
+    }
+}
